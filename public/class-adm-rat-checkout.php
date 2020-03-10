8038a44a96b5001baab936b29815d4fb7af15908
Preciousomonze <crispusomonze@gmail.com>
2020-03-10 17:34:15 +0100

    update: only add meta value when value is 'true'

--- public/class-adm-rat-checkout.php
+++ public/class-adm-rat-checkout.php
@@ -20,17 +20,20 @@ Class ADM_RAT_Checkout{
 	 * @param WC_Order              $order
  	 */
 	public function add_request_tailor_data_to_order_items( $item, $cart_item_key, $values, $order ) {
+		var_dump($item);
 		$cart_item_data = adm_pk_woo_get_item_data($cart_item_key);
 		$request_value = isset( $cart_item_data['_adm_request_tailor'] ) ? trim( $cart_item_data['_adm_request_tailor'] ) : '';
 		if ( empty( $request_value ) ) {
 			return;
 		}
-
-		$cloth_type = '<span class="adm-cloth-type">'.$cart_item_data['_cloth_type'].'</span>';
-		$value = '<span class="adm-unit">'.( $request_value === true ? 'Yes' : 'No' ).'</span>';
-		//done now add :)
-		$item->add_meta_data( __( '_request_tailor', ADM_RAT_TEXT_DOMAIN ), $value);
 		
+		// Only add when its true.
+		if( $request_value === 'true' ){
+			//$cloth_type = '<span class="adm-cloth-type">'.$cart_item_data['_cloth_type'].'</span>';
+			$value = '<span class="adm-unit">'.( $request_value === 'true' ? 'Yes' : 'No' ).'</span>';
+			//done now add :)
+			$item->add_meta_data( __( '_request_tailor', ADM_RAT_TEXT_DOMAIN ), $value );
+		}
 		// hook filter since request tailor has been in it
 		/**
 		 * A template like function which sets bool value on item for request tailor
